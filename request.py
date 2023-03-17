8d2ee1ca5cf9c4a417968e6a366e57a9aa247869
khatiri1378 <77232380+khatiri1378@users.noreply.github.com>
2023-03-17 14:36:17 +0330

    Update request.py

--- request.py
+++ request.py
@@ -1,18 +1,13 @@
 from pyodide.http import pyfetch, FetchResponse
 from typing import Optional, Any
 
-async def request(url: str, method: str = "GET", body: Optional[str] = None,
-                  headers: Optional[dict[str, str]] = None, **fetch_kwargs: Any) -> FetchResponse:
-                  
-                  
-                  kwargs = {"method": method, "mode": "cors"} 
-                  if body and method not in ["GET", "HEAD"]:
-                      kwargs["body"] = body
-                      if headers:
-                          
-                          kwargs["headers"] = headers
-                          kwargs.update(fetch_kwargs)
-
-                          response = await pyfetch(url, **kwargs)
-                          return response
+async def request(url: str, method: str = "GET", body: Optional[str] = None, headers: Optional[dict[str, str]] = None, **fetch_kwargs: Any) -> FetchResponse:
+  kwargs = {"method": method, "mode": "cors"} 
+  if body and method not in ["GET", "HEAD"]:
+    kwargs["body"] = body
+    if headers:
+      kwargs["headers"] = headers
+      kwargs.update(fetch_kwargs)
+      response = await pyfetch(url, **kwargs)
+      return response
 asyncio.ensure_future(request())
