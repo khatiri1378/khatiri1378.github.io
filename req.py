6d42c91c8f08441acbc91b6a9bd838731969bd93
khatiri1378 <77232380+khatiri1378@users.noreply.github.com>
2023-03-18 18:45:33 +0000

    noway

--- req.py
+++ req.py
@@ -1,7 +1,11 @@
 import urllib.request
+import ssl
 
 def get():
     url = "https://www.google.com"
-    response = urllib.request.urlopen(url)
+    context = ssl.create_default_context()
+    handler = urllib.request.HTTPSHandler(context=context)
+    opener = urllib.request.build_opener(handler)
+    response = opener.open(url)
     print(response.code)
 get()
