1bc72b7ee4756d845d9164693e70e89c2ef8eee7
khatiri1378 <77232380+khatiri1378@users.noreply.github.com>
2023-03-18 19:07:32 +0000

    newhope

--- req.py
+++ req.py
@@ -1,13 +1,27 @@
-import urllib.request
-import ssl
+from pyodide.http import pyfetch, FetchResponse
+from typing import Optional, Any
 
-def get():
-    url = "https://www.google.com"
-    context = ssl.create_default_context()
-    context.set_ciphers('DEFAULT:@SECLEVEL=1')
-    context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
-    handler = urllib.request.HTTPSHandler(context=context)
-    opener = urllib.request.build_opener(handler)
-    response = opener.open(url)
-    print(response.code)
-get()
+async def request(url: str, method: str = "GET", body: Optional[str] = None, headers: Optional[dict[str, str]] = None, **fetch_kwargs: Any) -> FetchResponse:
+    
+
+    """
+    Async request function. Pass in Method and make sure to await!
+    Parameters:
+        url: str = URL to make request to
+        method: str = {"GET", "POST", "PUT", "DELETE"} from `JavaScript` global fetch())
+        body: str = body as json string. Example, body=json.dumps(my_dict)
+        headers: dict[str, str] = header as dict, will be converted to string...
+            Example, headers=json.dumps({"Content-Type": "application/json"})
+        fetch_kwargs: Any = any other keyword arguments to pass to `pyfetch` (will be passed to `fetch`)
+    Return:
+        response: pyodide.http.FetchResponse = use with .status or await.json(), etc.
+    """
+    kwargs = {"method": method, "mode": "cors"}  # CORS: https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
+    if body and method not in ["GET", "HEAD"]:
+        kwargs["body"] = body
+    if headers:
+        kwargs["headers"] = headers
+    kwargs.update(fetch_kwargs)
+
+    response = await pyfetch(url, **kwargs)
+    return response
