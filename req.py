88a0216575dd1aa24b8b5f7295d098fb6f2a9067
khatiri1378 <77232380+khatiri1378@users.noreply.github.com>
2023-03-18 18:26:42 +0000

    ggg

--- req.py
+++ req.py
@@ -1,7 +1,10 @@
 import requests
+import urllib3
+
 def get():
 
-    x = requests.get("https://www.google.com", verify="cacert.pem")
+    x = requests.get("https://www.google.com")
+    urllib3.disable_warnings()
     display(x.status_code)
 
 
