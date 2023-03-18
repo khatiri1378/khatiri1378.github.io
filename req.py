7622e0b5a0b14af1548b3f6d473e75d0971c0609
khatiri1378 <77232380+khatiri1378@users.noreply.github.com>
2023-03-18 18:32:48 +0000

    hhh

--- req.py
+++ req.py
@@ -1,9 +1,10 @@
+import ssl
 import requests
 import urllib3
 
 def get():
 
-    x = requests.get("https://www.google.com")
+    x = requests.get("https://www.google.com", verify=False)
     urllib3.disable_warnings()
     display(x.status_code)
 
