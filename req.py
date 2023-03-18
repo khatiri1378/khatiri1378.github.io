fd365c95dda0f9901380ee09b29cdb71be6c5dd6
khatiri1378 <77232380+khatiri1378@users.noreply.github.com>
2023-03-18 18:53:27 +0000

    heklo

--- req.py
+++ req.py
@@ -4,6 +4,8 @@ import ssl
 def get():
     url = "https://www.google.com"
     context = ssl.create_default_context()
+    context.set_ciphers('DEFAULT:@SECLEVEL=1')
+    context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
     handler = urllib.request.HTTPSHandler(context=context)
     opener = urllib.request.build_opener(handler)
     response = opener.open(url)
