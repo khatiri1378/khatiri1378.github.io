89002020563dcbe92f7ac19ba9ff767145a1121d
khatiri1378 <77232380+khatiri1378@users.noreply.github.com>
2023-03-19 17:30:57 +0000

    hope

--- test.py
+++ test.py
@@ -1,4 +1,28 @@
+import pycurl
 
-import os
-print(os.system("curl https://raw.githubusercontent.com/khatiri1378/khatiri1378.github.io/main/test.txt"))
+# Create a new curl object
+curl = pycurl.Curl()
 
+# Set the URL to make the request to
+curl.setopt(pycurl.URL, "http://www.google.com")
+
+# Set any additional options you need, such as headers or data to send in the request
+curl.setopt(pycurl.HTTPHEADER, ["Content-Type: application/json"])
+
+# Set a callback function to handle the response
+response = []
+def handle_response(data):
+    response.append(data)
+
+curl.setopt(pycurl.WRITEFUNCTION, handle_response)
+
+# Perform the request
+curl.perform()
+
+# Check the response code and any response data
+http_code = curl.getinfo(pycurl.RESPONSE_CODE)
+print(f"Response code: {http_code}")
+
+
+# Clean up
+curl.close()
